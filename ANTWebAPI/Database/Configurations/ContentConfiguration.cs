b30387110a13c05149d915c20f44d2db604bb952
FredNekrasov <frednekrasov49@gmail.com>
2025-02-03 11:34:17 +0300

    creating entity configuration classes

---
+++ ANTWebAPI/Database/Configurations/ContentConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ANTWebAPI.Database.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANTWebAPI.Database.Configurations;
+
+internal class ContentConfiguration : IEntityTypeConfiguration<Content>
+{
+    public void Configure(EntityTypeBuilder<Content> entity)
+    {
+        entity.ToTable("ANTContents")
+            .HasKey(t => t.Id);
+        
+        entity.Property(t => t.Id)
+            .HasColumnName("content_id")
+            .IsRequired();
+        
+        entity.Property(t => t.ArticleId)
+            .HasColumnName("article_id")
+            .IsRequired();
+        
+        entity.Property(t => t.Data)
+            .HasColumnName("data")
+            .IsRequired();
+    }
+}
