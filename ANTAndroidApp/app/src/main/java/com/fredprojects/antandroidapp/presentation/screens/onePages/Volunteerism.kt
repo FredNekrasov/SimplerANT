ab52fb101da0ae42e62aefb9766773fa4efc36bc
FredNekrasov <frednekrasov49@gmail.com>
2025-02-03 10:20:44 +0300

    designing screens

---
+++ ANTAndroidApp/app/src/main/java/com/fredprojects/antandroidapp/presentation/screens/onePages/Volunteerism.kt
@@ -0,0 +1,32 @@
+package com.fredprojects.antandroidapp.presentation.screens.onePages
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import com.fredprojects.ant.presentation.core.ANTStrings
+import com.fredprojects.antandroidapp.presentation.core.*
+import com.fredprojects.antandroidapp.presentation.core.components.ImageSlider
+import com.fredprojects.antandroidapp.presentation.screens.vm.MainArticleState
+
+@Composable
+fun Volunteerism(
+    state: MainArticleState,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier.verticalScroll(rememberScrollState()).padding(8.dp)) {
+        state.list.fastForEach {
+            if(it.catalog.name != ANTStrings.VOLUNTEERISM) return@fastForEach
+            FredTitle(it.catalog.name)
+            Spacer(Modifier.height(8.dp))
+            FredTitle(it.title)
+            Spacer(Modifier.height(4.dp))
+            FredText(it.description)
+            Spacer(Modifier.height(8.dp))
+            ImageSlider(it, Modifier.fillMaxWidth().aspectRatio(1.0f))
+        }
+    }
+}
