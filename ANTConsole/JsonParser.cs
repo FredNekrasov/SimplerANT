d3ec6704e28fef056222f8eb45f8193024a4e9bf
FredNekrasov <frednekrasov49@gmail.com>
2025-02-05 20:15:53 +0300

    adding comments to the JsonParser.cs

--- ANTConsole/JsonParser.cs
+++ ANTConsole/JsonParser.cs
@@ -4,6 +4,13 @@ namespace ANTConsole;
 
 public static class JsonParser<T>
 {
+    /// <summary>
+    /// Parses JSON data from a specified file and returns a list of objects of type T.
+    /// If the file does not exist or an error occurs during parsing, it returns null.
+    /// </summary>
+    /// <param name="filePath">The path to the JSON file to be parsed.</param>
+    /// <param name="jsonSerializerOptions">Options to customize the JSON deserialization process.</param>
+    /// <returns>A list of objects of type T, or null if parsing fails.</returns>
     public static List<T>? GetParsedListOrNull(string filePath, JsonSerializerOptions jsonSerializerOptions)
     {
         try
